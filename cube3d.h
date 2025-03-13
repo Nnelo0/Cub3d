19ba06ee1184858f6ac4a1fafe59e72e6bddeb52
Leo Langlois <lelanglo@c2r10p4.42lehavre.fr>
2025-03-13 10:48:45 +0100

    p

--- cube3d.h
+++ cube3d.h
@@ -6,12 +6,19 @@
 /*   By: lelanglo <lelanglo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/13 09:03:21 by lelanglo          #+#    #+#             */
-/*   Updated: 2025/03/13 09:04:37 by lelanglo         ###   ########.fr       */
+/*   Updated: 2025/03/13 10:33:30 by lelanglo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "minilibx-linux/mlx.h"
 # include <stdlib.h>
-# include "libft/libft.h"
+# include "libft/libft/libft.h"
+# include "libft/printf/ft_printf.h"
 # include <stdbool.h>
+# include <math.h>
 
+typedef struct data
+{
+	void	*mlx;
+	void	*window;
+} t_data;
