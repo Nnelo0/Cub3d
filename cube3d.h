f3273e248bf967b5a2442a036b8505a39509ea2c
Leo Langlois <lelanglo@c2r10p4.42lehavre.fr>
2025-03-13 15:26:43 +0100

    push

--- cube3d.h
+++ cube3d.h
@@ -6,9 +6,12 @@
 /*   By: lelanglo <lelanglo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/13 09:03:21 by lelanglo          #+#    #+#             */
-/*   Updated: 2025/03/13 10:33:30 by lelanglo         ###   ########.fr       */
+/*   Updated: 2025/03/13 11:10:35 by lelanglo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#ifndef CUBE3D_H
+# define CUBE3D_H
 
 # include "minilibx-linux/mlx.h"
 # include <stdlib.h>
@@ -21,4 +24,7 @@ typedef struct data
 {
 	void	*mlx;
 	void	*window;
-} t_data;
+	char	**map;
+}	t_data;
+
+#endif
