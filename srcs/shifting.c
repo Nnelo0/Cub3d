963f47913a380fef2aac3cfa9f98da8ff58afb72
Leo Langlois <lelanglo@c2r10p4.42lehavre.fr>
2025-03-19 10:57:25 +0100

    collisions

--- srcs/shifting.c
+++ srcs/shifting.c
@@ -6,7 +6,7 @@
 /*   By: lelanglo <lelanglo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/18 13:29:11 by lelanglo          #+#    #+#             */
-/*   Updated: 2025/03/18 14:56:02 by lelanglo         ###   ########.fr       */
+/*   Updated: 2025/03/19 10:53:32 by lelanglo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,13 +42,37 @@ void	turn(t_data *data, int sens)
 void	move(t_data *data, int sens)
 {
 	destroy_image(data);
-	data->player.x += data->player.dir_x * (double)SPEED * sens;
-	data->player.y += data->player.dir_y * (double)SPEED * sens;
+	if (sens == 1)
+	{
+		if (data->map[(int)data->player.y][(int)(data->player.x + data->player.dir_x * (double)SPEED)] == '0')
+			data->player.x += data->player.dir_x * (double)SPEED;
+		if (data->map[(int)(data->player.y + data->player.dir_y * (double)SPEED)][(int)data->player.x] == '0')
+			data->player.y += data->player.dir_y * (double)SPEED;
+	}
+	else if (sens == -1)
+	{
+		if (data->map[(int)data->player.y][(int)(data->player.x - data->player.dir_x * (double)SPEED)] == '0')
+			data->player.x -= data->player.dir_x * (double)SPEED;
+		if (data->map[(int)(data->player.y - data->player.dir_y * (double)SPEED)][(int)data->player.x] == '0')
+			data->player.y -= data->player.dir_y * (double)SPEED;
+	}
 }
 
 void	left_right(t_data *data, int sens)
 {
 	destroy_image(data);
-	data->player.x += data->player.plane_x * (double)SPEED * sens;
-	data->player.y += data->player.plane_y * (double)SPEED * sens;
+	if (sens == 1)
+	{
+		if (data->map[(int)data->player.y][(int)(data->player.x + data->player.plane_x * (double)SPEED)] == '0')
+			data->player.x += data->player.plane_x * (double)SPEED * sens;
+		if (data->map[(int)(data->player.y + data->player.plane_y * (double)SPEED)][(int)data->player.x] == '0')
+			data->player.y += data->player.plane_y * (double)SPEED * sens;
+	}
+	else if (sens == -1)
+	{
+		if (data->map[(int)data->player.y][(int)(data->player.x - data->player.plane_x * (double)SPEED)] == '0')
+			data->player.x -= data->player.plane_x * (double)SPEED;
+		if (data->map[(int)(data->player.y - data->player.plane_y * (double)SPEED)][(int)data->player.x] == '0')
+			data->player.y -= data->player.plane_y * (double)SPEED;
+	}
 }
